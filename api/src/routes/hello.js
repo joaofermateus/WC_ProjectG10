9889173f74df6b6149cedbb6f265974044675426
DavidSousa <davidbdsousa@gmail.com>
2015-12-23 17:49:49 +0000

    mensagem

---
+++ api/src/routes/hello.js
@@ -0,0 +1,20 @@
+var server = require('./../index.js')
+var resources = require('./../resources')
+
+server.route({
+  method: 'GET',
+  path: '/hello',
+  handler: resources.hello.get
+})
+
+server.route({
+  method: 'POST',
+  path: '/hello',
+  handler: resources.hello.post
+})
+
+server.route({
+	method: 'POST',
+	path: '/login',
+	handler: resources.hello.login
+})
